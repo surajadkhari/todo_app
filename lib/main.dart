07dda10c6895d57afa2f33a9c9c309fc8416a57c
surajadkhari <surajadkhari@gmail.com>
2021-03-13 16:25:29 +0545

    todo app

--- lib/main.dart
+++ lib/main.dart
@@ -1,117 +1,132 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(TodoApp());
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class TodoApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    return MaterialApp(title: 'Todo List', home: TodoList());
+  }
+}
+
+class Todo {
+  int id;
+  String title;
+  DateTime date;
+
+  Todo({this.id, this.title, this.date});
+}
+
+class TodoList extends StatefulWidget {
+  @override
+  createState() => TodoListState();
+}
+
+class TodoListState extends State<TodoList> {
+  List<Todo> _todoList = [];
+
+  void _addTodoItem(Todo task) {
+    if (task.title != null) {
+      setState(() {
+        _todoList.add(task);
+        _todoList.sort((a, b) => b.date.compareTo(a.date));
+      });
+    }
+  }
+
+  void _editTodoItem(Todo task, int index) {
+    if (task.title != null) {
+      setState(() {
+        _todoList.removeAt(index);
+        _todoList.add(task);
+        _todoList.sort((a, b) => b.date.compareTo(a.date));
+      });
+    }
+  }
+
+  void _removeTodoItem(int index) {
+    setState(() => _todoList.removeAt(index));
+  }
+
+  // Build the whole list of todo items
+  Widget _buildTodoList() {
+    return ListView.builder(
+      itemBuilder: (context, index) {
+        if (index < _todoList.length) {
+          return _buildTodoItem(_todoList[index], index);
+        }
+      },
     );
   }
-}
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  // Build a single todo item
+  Widget _buildTodoItem(Todo todoText, int index) {
+    return ListTile(
+      title: Text(todoText.title),
+      subtitle: Text(todoText.date.toString().substring(0, 19)),
+      trailing: PopupMenuButton(
+        itemBuilder: (context) {
+          return [
+            PopupMenuItem(
+              child: Text("Edit"),
+              value: 'edit',
+            ),
+            PopupMenuItem(
+              child: Text("Delete"),
+              value: 'delete',
+            )
+          ];
+        },
+        onSelected: (value) {
+          switch (value) {
+            case "edit":
+              {
+                pushAddTodoScreen(todoText, 'Edit todo', index);
+              }
+              break;
+            case "delete":
+              {
+                _removeTodoItem(index);
+              }
+              break;
+          }
+        },
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
+      appBar: AppBar(title: Text('Todo List ')),
+      body: _buildTodoList(),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+          onPressed: () => pushAddTodoScreen(Todo(title: ''), 'Add Todo', 0),
+          tooltip: 'Add task',
+          child: Icon(Icons.add)),
     );
+  }
+
+  pushAddTodoScreen(Todo todo, String title, int index) {
+    Navigator.of(context).push(MaterialPageRoute(builder: (context) {
+      TextEditingController _todoController =
+          TextEditingController(text: todo.title ?? '');
+      return Scaffold(
+          appBar: AppBar(title: Text(title ?? 'Add a new task')),
+          body: TextField(
+            autofocus: true,
+            controller: _todoController,
+            onSubmitted: (val) {
+              if (index != 0)
+                _editTodoItem(Todo(date: DateTime.now(), title: val), index);
+              else
+                _addTodoItem(Todo(date: DateTime.now(), title: val));
+              Navigator.pop(context); // Close the add todo screen
+            },
+            decoration: InputDecoration(
+                hintText: 'Enter something to do...',
+                contentPadding: const EdgeInsets.all(16.0)),
+          ));
+    }));
   }
 }
